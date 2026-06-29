612f0a3b8457b8c39b00d7d7706233f5be2d0cc7
ashfaq <ashfaqjutt1000000@gmail.com>
2026-06-29 14:22:15 -0700

    Cors fix

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ const allowedOrigins = [
   "https://ffkitchen.com",
   "http://localhost:5174",
   "https://ffkitchen-clientside.vercel.app",
-  "https://ffkitchen-admin-frontend.vercel.app",
+  "https://ffkitchen-admin-frontend-x1l4-aon93df6q-ashfaqdev1s-projects.vercel.app",
 ];
 
 const corsOptions = {
